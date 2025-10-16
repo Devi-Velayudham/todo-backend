1149bd9e515293cbb1afca07620b678fc7bc431c
Devi-Velayudham <dev.peridot@gmail.com>
2025-10-17 01:37:22 +0530

    Final Fix: Corrected CORS middleware order

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ const app = express();
 //app.use(cors());
 app.use(express.json());
 
+const corsOptions = {
+    origin: "https://todo-frontend-app-bgpi.onrender.com", // React app
+    credentials: true,
+};
+
+app.use(cors(corsOptions));
+
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
@@ -20,12 +27,7 @@ mongoose.connect(process.env.MONGO_URI, {
 // Import Todo model
 const Todo = require("./models/Todo.js");
 
-const corsOptions = {
-  origin: "https://todo-frontend-app-bgpi.onrender.com", // React app
-  credentials: true,
-};
 
-app.use(cors(corsOptions));
 
 
 // Routes
