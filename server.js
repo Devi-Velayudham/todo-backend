a50142aae867f7b7fab28ba67cc797c4a212bb86
Devi-Velayudham <dev.peridot@gmail.com>
2025-10-22 23:04:22 +0530

    Mobile.

--- server.js
+++ server.js
@@ -30,13 +30,7 @@ const corsOptions = {
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors(corsOptions));
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Origin", "https://todo-frontend-app-bqpl.onrender.com");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+
 
 // --- Database Connection ---
 mongoose.connect(MONGO_URI)
